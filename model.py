97582af9d0afc3bbaacb88da98cc25879491931f
Sahariar Hasan <68711966+solo938@users.noreply.github.com>
2026-07-25 14:28:54 +0530

    Step 8: trainable_fraction

--- model.py
+++ model.py
@@ -84,8 +84,11 @@ def count_trainable_parameters(model):
 
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-# Step 8 - trainable_fraction (not yet solved)
-# TODO: implement
+# Step 8 - trainable_fraction
+def trainable_fraction(trainable_count, total_count):
+    # TODO: return the fraction of parameters that are trainable.
+   trainable_fraction = trainable_count / total_count
+   return(trainable_fraction)
 
 # Step 9 - build_instruction_examples (not yet solved)
 # TODO: implement
