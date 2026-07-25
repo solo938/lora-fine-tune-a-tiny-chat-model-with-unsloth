dc8d9e42fed9d7e6c03a1353c25d7c34cdddc6e9
Sahariar Hasan <68711966+solo938@users.noreply.github.com>
2026-07-25 13:49:32 +0530

    Step 5: get_lora_target_modules

--- model.py
+++ model.py
@@ -52,8 +52,13 @@ def ensure_pad_token(tokenizer):
         tokenizer.pad_token = tokenizer.eos_token
     return tokenizer
 
-# Step 5 - get_lora_target_modules (not yet solved)
-# TODO: implement
+# Step 5 - get_lora_target_modules
+def get_lora_target_modules():
+    """Return the attention projection module name suffixes for LoRA."""
+    # TODO: return the list of attention projection module names LoRA should adapt
+    target_modules = ['q_proj', 'k_proj', 'v_proj', 'o_proj']
+    
+    return target_modules
 
 # Step 6 - attach_lora_adapters (not yet solved)
 # TODO: implement
